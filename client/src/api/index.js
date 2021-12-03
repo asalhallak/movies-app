dd5c756f475653846d55d04a9810b26efa25f79e
Abdullah Al-Hallak <a.al-hallak@sensory-minds.com>
2021-12-03 18:34:39 +0100

    Simple config

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
-
+const isProduction = process && process.env && process.env.NODE_ENV === 'production'
+console.log('isProduction: ', isProduction)
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL:  isProduction ? 'http://portal.movie-app.com/api' : 'http://localhost:3000/api'
 })
-
 export const insertMovie = payload => api.post(`/movie`, payload)
 export const getAllMovies = () => api.get(`/movies`)
 export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
 export const deleteMovieById = id => api.delete(`/movie/${id}`)
 export const getMovieById = id => api.get(`/movie/${id}`)
+
 
 const apis = {
     insertMovie,
